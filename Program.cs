9290ffa2ca749e2fd5b99216bb6b0dace64b7f66
TianJingsolo <1549153348@qq.com>
2017-10-29 13:19:57 +0800

    assignment
    ...

--- Program.cs
+++ Program.cs
@@ -9,50 +9,39 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            String str1, str2, str3,str;
-            Console.Write("Please input the first name: ");
-            str1=Console.ReadLine();
-            if (str1[0] < 'A' || str1[0] > 'Z' && str1[0] < 'a' || str1[0] > 'z')
+            int num=3,i,j;
+            String[] str = new String[num];
+            String temp_str;
+            for (i = 0; i < num; i++)
             {
-                Console.WriteLine("The first name : Error!");
-                System.Environment.Exit(0);
+                Console.Write("Please input the ");
+                Console.Write(i+1);
+                Console.Write("'th name: ");
+                str[i] = Console.ReadLine();
+                if (str[i][0] < 'A' || str[i][0] > 'Z' && str[i][0] < 'a' || str[i][0] > 'z')
+                {
+                    Console.WriteLine("Error!");
+                    System.Environment.Exit(0);
+                }
+                str[i] = str[i].Substring(0, 1).ToUpper() + str[i].Substring(1, str[i].Length - 1).ToLower();
+                Console.Write(str[i]);
+                Console.Write(" is the ");
+                Console.Write(i+1);
+                Console.WriteLine("'th name");
             }
-            Console.Write("Please input the second name: ");
-            str2=Console.ReadLine();
-            if (str2[0] < 'A' || str2[0] > 'Z' && str2[0] < 'a' || str2[0] > 'z')
-            {
-                Console.WriteLine("The second name : Error!");
-                System.Environment.Exit(0);
-            }
-            Console.Write("Please input the third name: ");
-            str3 = Console.ReadLine();
-            if (str3[0] < 'A' || str3[0] > 'Z' && str3[0] < 'a' || str3[0] > 'z')
-            {
-                Console.WriteLine("The third name : Error!");
-                System.Environment.Exit(0);
-            }
-            if (str1.CompareTo(str2) > 0)
-            {
-                str = str1;
-                str1 = str2;
-                str2 = str;
-            }
-            if (str1.CompareTo(str3) > 0)
-            {
-                str = str1;
-                str1 = str3;
-                str3 = str;
-            }
-            if (str2.CompareTo(str3) > 0)
-            {
-                str = str2;
-                str2 = str3;
-                str3 = str;
-            }
+            for(i=0;i<num-1;i++)
+                for(j=i+1;j<num;j++)
+                {
+                    if(str[i].CompareTo(str[j])>0)
+                    {
+                        temp_str=str[i];
+                        str[i]=str[j];
+                        str[j]=temp_str;
+                    }
+                }
             Console.WriteLine("The names in alphabetical order are: ");
-            Console.WriteLine("\""+str1.Substring(0,1).ToUpper()+str1.Substring(1,str1.Length-1).ToLower()+"\"");
-            Console.WriteLine("\""+str2.Substring(0,1).ToUpper()+str2.Substring(1,str2.Length-1).ToLower()+"\"");
-            Console.WriteLine("\""+str3.Substring(0,1).ToUpper()+str3.Substring(1,str3.Length-1).ToLower()+"\"");
+            for(i=0;i<num;i++)
+                Console.WriteLine("\"" +str[i] + "\"");
         }
     }
 }
